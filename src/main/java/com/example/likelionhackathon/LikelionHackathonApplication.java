af47710c6584ac18272a4d8d4943938d3815473b
taesungs <70329288+taesungs@users.noreply.github.com>
2026-08-09 17:26:17 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/example/likelionhackathon/LikelionHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionhackathon/LikelionHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
