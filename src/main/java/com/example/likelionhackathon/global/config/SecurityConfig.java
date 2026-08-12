c0e8a69c98e7a089ff823556382a08a36c4b918f
taesungs <70329288+taesungs@users.noreply.github.com>
2026-08-12 16:09:49 +0900

    fix: SecurityConfig 권한 설정 변경 (403 오류 해결)

--- src/main/java/com/example/likelionhackathon/global/config/SecurityConfig.java
+++ src/main/java/com/example/likelionhackathon/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(SWAGGER_URLS).permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
